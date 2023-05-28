28ddbc094492d01af8c5d0769a3e89a7619cd640
Ravshanbekk07 <ramazonof07@gmail.com>
2023-05-28 19:40:16 +0500

    cool

--- social-number.py
+++ social-number.py
@@ -12,4 +12,18 @@ class SocialNumber(Randommer):
         Returns:
             str: number as str
         '''
-        pass
+        endpoint = 'SocialNumber'
+        url   = self.get_url()+endpoint
+        headers ={
+            'X-Api-Key' : api_key
+        }
+        response = requests.get(url=url,headers=headers)
+        if response.status_code ==200:
+            return response.json()
+        return False
+    
+
+
+TOKEN = 'c0410fcc5b554a67a6611b1c235849c6'
+social = SocialNumber()
+print(social.get_SocialNumber(api_key=TOKEN)) 
