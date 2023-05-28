451fa426b21b445561c95940803d195a30b5757d
Ravshanbekk07 <ramazonof07@gmail.com>
2023-05-28 19:34:28 +0500

    cool

--- finance.py
+++ finance.py
@@ -11,7 +11,17 @@ class Finance(Randommer):
         Returns:
             list: list of types
         '''
-        pass
+        endpoint = 'Finance/CryptoAddress/Types'
+        url   = self.get_url()+endpoint
+        headers ={
+            'X-Api-Key' : api_key
+        }
+        response = requests.get(url=url,headers=headers)
+        if response.status_code ==200:
+            return response.json()
+        return False
+
+
 
     def get_crypto_address(self, crypto_type: str, api_key: str) -> dict:
         '''get available crypto address
@@ -23,7 +33,18 @@ class Finance(Randommer):
         Returns:
             dict: crypto address
         '''
-        pass
+        endpoint = 'Finance/CryptoAddress'
+        url   = self.get_url()+endpoint
+        payload={
+            'crypto_type':str
+        }
+        headers ={
+            'X-Api-Key' : api_key
+        }
+        response = requests.get(url=url,headers=headers,params=payload)
+        if response.status_code ==200:
+            return response.json()
+        return False
 
     def get_countries(self, api_key: str) -> list:
         '''get available countries
@@ -34,7 +55,16 @@ class Finance(Randommer):
         Returns:
             list: crypto address
         '''
-        pass
+        endpoint = 'Finance/Countries'
+        url   = self.get_url()+endpoint
+        
+        headers ={
+            'X-Api-Key' : api_key
+        }
+        response = requests.get(url=url,headers=headers)
+        if response.status_code ==200:
+            return response.json()
+        return False
 
     def get_iban_by_country_code(self, country_code: str, api_key: str) -> dict:
         '''get available countries
@@ -47,3 +77,19 @@ class Finance(Randommer):
             dict: idan data
         '''
         pass
+
+
+
+
+
+
+
+
+
+
+
+fin =Finance()
+TOKEN = 'c0410fcc5b554a67a6611b1c235849c6'
+#print(fin.get_crypto_address_types(api_key=TOKEN))
+#print(fin.get_crypto_address(api_key=TOKEN,crypto_type=str))
+print(fin.get_countries(api_key=TOKEN))
