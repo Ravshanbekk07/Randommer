fede53d27309ea7ecc99b101e57694e61490a4cb
Ravshanbekk07 <ramazonof07@gmail.com>
2023-05-28 19:36:33 +0500

    cool

--- misc.py
+++ misc.py
@@ -12,8 +12,15 @@ class Misc(Randommer):
         Returns:
             list: list of cultures
         '''
-        pass
-    
+        endpoint = 'Misc/Cultures'
+        url   = self.get_url()+endpoint
+        headers ={
+            'X-Api-Key' : api_key
+        }
+        response = requests.get(url=url,headers=headers)
+        if response.status_code ==200:
+            return response.json()
+        return False
     def get_random_address(self, api_key: str, number: int, culture='en') -> list:
         '''get available misc cultures
 
@@ -25,4 +32,31 @@ class Misc(Randommer):
         Returns:
             list: random address
         '''
-        pass
+        endpoint = 'Misc/Random-Address'
+        url   = self.get_url()+endpoint
+        payload = {
+            'number':2,
+            'culture':'en'
+        }
+        headers ={
+            'X-Api-Key' : api_key
+        }
+        response = requests.get(url=url,headers=headers,params=payload)
+        if response.status_code ==200:
+            return response.json()
+        return False
+
+misc =Misc()
+TOKEN = 'c0410fcc5b554a67a6611b1c235849c6'
+#print(misc.get_cultures(api_key=TOKEN))
+print(misc.get_random_address(api_key=TOKEN,number=int,culture='en'))
+
+
+
+
+
+
+
+
+
+
